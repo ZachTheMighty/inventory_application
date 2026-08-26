76d9803500ade60fb27c1140b660ebd14695d188
ZachTheMighty <promoted66@gmail.com>
2026-08-26 20:05:45 +0300

    add: validator to add category form

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,3 +1,4 @@
+const { body, validationResult, matchedData } = require("express-validator");
 const db = require("../db/queries.js");
 
 const categoriesListGet = async (req, res) => {
@@ -6,13 +7,31 @@ const categoriesListGet = async (req, res) => {
 };
 
 const categoriesCreateGet = async (req, res) => {
-  res.render("createCategory.ejs", { added: false });
+  res.render("createCategory.ejs", { added: false, errors: [] });
 };
 
-const categoriesCreatePost = async (req, res) => {
-  await db.insertCategory(req.body.categoryName);
-  res.render("createCategory.ejs", { added: true });
-};
+const validateCategory = [
+  body("categoryName")
+    .trim()
+    .notEmpty()
+    .withMessage("field can't be empty")
+    .isLength({ max: 20 })
+    .withMessage("name can't be over 20 chars"),
+];
+
+const categoriesCreatePost = [
+  validateCategory,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res
+        .status(400)
+        .render("createCategory.ejs", { added: false, errors });
+
+    await db.insertCategory(matchedData(req.body.categoryName));
+    res.render("createCategory.ejs", { added: true, errors: [] });
+  },
+];
 
 const instrumentsListGet = async (req, res) => {
   const instruments = await db.getInstruments(req.params.categoryName);
